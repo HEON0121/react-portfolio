49162493c2b20e126d2d8f1cf76f993d06f9512e
seheon Kim <seheon.emma@gmail.com>
2022-09-27 10:43:41 +1000

    test commit

--- sangamzombie_front/src/App.js
+++ sangamzombie_front/src/App.js
@@ -1,25 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+function Nav() {
+  return 
+  <nav class="navbar">
+    <div class="navbar_logo">
+        <i class="fa-solid fa-washing-machine"></i>
+        <a href="">Laundry Service</a>    
+    </div>
+    <ul class="navbar_menu">
+        <li><a href="https://ko.reactjs.org/tutorial/tutorial.html">kim's laundry service</a></li>
+        <li><a href="https://ko.reactjs.org/tutorial/tutorial.html">이용방법</a></li>
+        <li><a href="https://ko.reactjs.org/tutorial/tutorial.html">이벤트/공지</a></li>
+        <li><a href="https://ko.reactjs.org/tutorial/tutorial.html">회사 소개</a></li>
+    </ul>
+    <div class="navbar_links">
 
+    </div>
+  </nav>
+
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <nav class="navbar">
+    <div class="navbar_logo">
+        <i class="fa-solid fa-washing-machine"></i>
+        <a href="https://ko.reactjs.org/tutorial/tutorial.html">Laundry Service</a>    
     </div>
+    <ul class="navbar_menu">
+        <li><a href="https://ko.reactjs.org/tutorial/tutorial.html">kim's laundry service</a></li>
+        <li><a href="https://ko.reactjs.org/tutorial/tutorial.html">이용방법</a></li>
+        <li><a href="https://ko.reactjs.org/tutorial/tutorial.html">이벤트/공지</a></li>
+        <li><a href="https://ko.reactjs.org/tutorial/tutorial.html">회사 소개</a></li>
+    </ul>
+    <div class="navbar_links">
+
+    </div>
+  </nav>
   );
+
+  
 }
 
 export default App;
